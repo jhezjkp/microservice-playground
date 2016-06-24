bd072e76b51332047af9a004b9af9b87f9a6a3d4
jhezjkp <jhezjkp@163.com>
2016-06-24 19:47:50 +0800

    增加服务器初始化配置

--- terraform/venus.tf
+++ terraform/venus.tf
@@ -26,5 +26,15 @@ resource "google_compute_instance" "venus" {
         }
     }
 
-    //metadata_startup_script = "echo hello > /abc.txt"
+    // set up docker
+    metadata_startup_script = "curl -fsSL https://test.docker.com/ | sh"
+
+    // 输出ip等到本地
+    provisioner "local-exec" {
+        command = "echo ${google_compute_instance.venus.network_interface.0.access_config.0.assigned_nat_ip} > ip.txt"
+    }
+    provisioner "local-exec" {
+        // below command valid for os x
+        command = "sed -i '' '/^${google_compute_instance.venus.network_interface.0.access_config.0.assigned_nat_ip}/d' ~/.ssh/known_hosts"
+    }
 }
