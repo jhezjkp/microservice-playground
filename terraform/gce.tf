f83f77369fc17cba711dae759b4aae79ba5d6dd5
jhezjkp <jhezjkp@163.com>
2016-07-01 19:27:58 +0800

    自动更新域名记录(已有对应动态dns记录)

--- terraform/gce.tf
+++ terraform/gce.tf
@@ -18,6 +18,10 @@ variable "instance_count" {
     default = 4
 }
 
+variable "instance_user" {
+    default = "vivia"
+}
+
 resource "google_compute_instance" "gce" {
     count = "${var.instance_count}"
     name = "${lookup(var.instance_names, count.index)}"
@@ -43,23 +47,38 @@ resource "google_compute_instance" "gce" {
 
     // ssh keys
     metadata {
-        ssh-keys = "root:${file("~/.ssh/id_rsa.pub")}"
+        ssh-keys = "vivia:${file("~/.ssh/id_rsa.pub")}"
     }
 
     // set up docker
-    metadata_startup_script = "curl -fsSL https://test.docker.com/ | sh"
+    // metadata_startup_script = "curl -fsSL https://test.docker.com/ | sh"
 
-    // 输出ip等到本地
-    provisioner "local-exec" {
-        command = "echo ${self.network_interface.0.access_config.0.assigned_nat_ip} ======"
+    provisioner "remote-exec" {
+        inline = [
+        // install docker(beta release)
+        "curl -fsSL https://test.docker.com/ | sh",
+        // add current user to docker group
+        "sudo usermod -aG docker `whoami`",
+        // install zsh
+        "sudo apt-get install -y zsh",
+        // configure behavior hangs, comment for now
+        // configure zsh with oh-my-zsh
+        //"sudo sh -c \"$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)\"",
+        // change to zsh
+        //"sudo chsh -s /bin/zsh `whoami`",
+        ]
+        connection {
+            user = "${var.instance_user}"
+            private_key = "${file("~/.ssh/id_rsa")}"
+        }
     }
     provisioner "local-exec" {
-        // below command valid for os x
-        command = "sed -i '' '/^${self.network_interface.0.access_config.0.assigned_nat_ip}/d' ~/.ssh/known_hosts"
+        // remove ssh fingerprint(valid for os x)
+        command = "ssh-keygen -R ${var.dnspod_domain}.vivia.me"
     }
     // 更新dnspod记录
     provisioner "local-exec" {
-        command = "curl -X POST https://dnsapi.cn/Record.Create -d 'login_token=${var.dnspod_login_token}&format=json&domain_id=${var.dnspod_domain_id}&sub_domain=${lookup(var.instance_names, count.index)}&record_type=A&record_line=默认&value=${self.network_interface.0.access_config.0.assigned_nat_ip}'"
+        command = "curl -X POST https://dnsapi.cn/Record.Ddns -d 'login_token=${var.dnspod_login_token}&format=json&domain_id=${var.dnspod_domain_id}&record_id=${lookup(var.dnspod_record_id, self.name)}&record_line=默认&sub_domain=${self.name}&value=${self.network_interface.0.access_config.0.assigned_nat_ip}'"
     }
 }
 
